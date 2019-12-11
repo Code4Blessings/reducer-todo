7f0ab50a8c6274fb763e9d72f663b9dc71ac6fd3
Code4Blessings <robinwarden@code4blessings.com>
2019-12-10 21:16:55 -0500

    Built TodoForm

--- reducer-todo/src/components/TodoForm.js
+++ reducer-todo/src/components/TodoForm.js
@@ -1,1 +1,36 @@
+import React, { useState } from 'react';
 
+
+const TodoForm = () => {
+    const [newTodo, setNewTodo] = useState("");
+
+    const handleChange = event => {
+        setNewTodo({ 
+            newTodo: event.target.value 
+        });
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        props.addToDo(newTodo);
+        setNewTodo({
+            newTodo: ""
+        })
+    }
+
+    return (
+        <form>
+             <label htmlFor="task">Task:
+                   <input 
+                   type="text" 
+                   placeholder="task"
+                   value={newTodo} 
+                   onChange={handleChange}
+                    />
+                </label>
+                <button type="submit">ADD</button>
+        </form>
+    );
+}
+
+export default TodoForm;
