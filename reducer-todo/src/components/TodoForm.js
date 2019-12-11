fb2149dc96008afe1959933f16bad9b456d4a93c
Code4Blessings <robinwarden@code4blessings.com>
2019-12-10 20:46:52 -0500

    Fixed Todo.js file

--- reducer-todo/src/components/TodoForm.js
+++ reducer-todo/src/components/TodoForm.js
@@ -1,13 +1,1 @@
-import React from "react";
-import './Todo.css';
 
-const Todo = (props) => {
-    return (
-        <div onClick={() => props.toggleComplete (props.todo.id)} className={`todo ${props.todo.completed ? ' completed' : ''}`}>
-        <p>{props.todo.task}</p>
-        
-        </div>
-    );
-}
-
-export default Todo;
